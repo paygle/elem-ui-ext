91c2a2d6df4f23849d635cff9660ff26ecccfbf2
paygle <dodar@qq.com>
2017-08-07 16:15:42 +0800

    删除多余

--- src/mixins/filters.js
+++ src/mixins/filters.js
@@ -4,8 +4,8 @@ export default {
      * 百分比过滤器
      */
     percent: function (value) {
-      if (isNaN(value)) return value
-      return Number(value)  * 1000000000 * 100 / 1000000000;
+      if (isNaN(value)) return value;
+      return Number(value) * 1000000000 * 100 / 1000000000;
     }
   }
-}
+};
